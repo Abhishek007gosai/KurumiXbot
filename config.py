675a0451db39ce84194f3884db94b9fcd8cd495b
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-06-16 14:09:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,11 +27,11 @@ SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "")
 PORT = os.environ.get("PORT", "8050")
 
 #Database --⚠️ REQUIRED--
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://gutsxrobot:4LfFzZZ4T3VpJzzW@cluster0.r4wywvl.mongodb.net/")
-DB_NAME = os.environ.get("DATABASE_NAME", "cluster0")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://gutsxrobot:4LfFzZZ4T3VpJzzW@Cluster0.r4wywvl.mongodb.net/")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
-VERIFY_DB = os.environ.get("VERIFY_DB", "mongodb+srv://kayaxrobot:yzSgHQLteJCTXUqK@cluster0.ltk8k8h.mongodb.net/?")
-DBV_NAME = os.environ.get("VERIFY_DBNAME", "cluster0")
+VERIFY_DB = os.environ.get("VERIFY_DB", "mongodb+srv://kayaxrobot:yzSgHQLteJCTXUqK@Cluster0.ltk8k8h.mongodb.net/?")
+DBV_NAME = os.environ.get("VERIFY_DBNAME", "Cluster0")
 
 
 TOKEN_PIC = os.environ.get("TOKEN_PIC", "")
