aefaae553fe6ff6c518d2da8441e2cc1c16d09da
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-06-21 16:59:13 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,6 +1,3 @@
-
-
-
 import time
 import pymongo, os
 import motor
@@ -63,8 +60,6 @@ class Rohit:
         self.rqst_fsub_data = self.database['request_forcesub']
         self.rqst_fsub_Channel_data = self.database['request_forcesub_channel']
         self.store_reqLink_data = self.database['store_reqLink']
-
-        self.premium_data = self.database['premium']
 
     # Shortener Token
     async def set_shortener_url(self, url):
