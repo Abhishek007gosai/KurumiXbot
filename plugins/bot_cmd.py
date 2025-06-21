7a9795a32615c375dd9463645e44f0bd63d4740c
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-06-21 18:45:52 +0530

    Update bot_cmd.py

--- plugins/bot_cmd.py
+++ plugins/bot_cmd.py
@@ -105,7 +105,7 @@ async def send_text(client: Bot, message: Message):
         # Final status update
         final_status = f"""<b>{complete_msg}
 
-<blockquote>ᴅᴏɴᴇ:</b> [{final_progress_bar}] {percent_complete:.0%}</blockquote>
+final_status = f"""<b>ᴅᴏɴᴇ:</b> [{final_progress_bar}] {percent_complete:.0%}</b>
 
 <b>🚻 ᴛᴏᴛᴀʟ ᴜsᴇʀs: <code>{total}</code>
 ✅ sᴜᴄᴄᴇssғᴜʟ: <code>{successful}</code>
